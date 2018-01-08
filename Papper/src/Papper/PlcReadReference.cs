23edf7602022c293a56c7145fcd5e15cd602450a
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2018-01-08 16:32:28 +0100

    minor refactoring

---
+++ Papper/src/Papper/PlcReadReference.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Papper
+{
+    public struct PlcReadReference : IPlcReference
+    {
+        public string Mapping { get; internal set; }
+        public string Variable { get; internal set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="address"> [Mapping].[Variable]</param>
+        /// <returns></returns>
+        public static PlcReadReference FromAddress(string address)
+        {
+            var firstDot = address.IndexOf('.');
+            var area = address.Substring(0, firstDot);
+            return new PlcReadReference
+            {
+                Mapping = area,
+                Variable = address.Substring(firstDot + 1)
+            };
+        }
+
+
+        public static IEnumerable<PlcReadReference> FromRoot(string root, params string[] variables)
+        {
+            foreach (var variable in variables)
+            {
+                yield return FromAddress($"{root}.{variable}");
+            }
+        }
+    }
+}
---
+++ Papper/src/Papper/PlcWriteReference.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Papper
+{
+
+    public struct PlcWriteReference : IPlcReference
+    {
+        public string Mapping { get; internal set; }
+        public string Variable { get; internal set; }
+        public object Value { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="address"> [Mapping].[Variable]</param>
+        /// <returns></returns>
+        public static PlcWriteReference FromAddress(string address, object value)
+        {
+            var firstDot = address.IndexOf('.');
+            var area = address.Substring(0, firstDot);
+            return new PlcWriteReference
+            {
+                Mapping = area,
+                Variable = address.Substring(firstDot + 1),
+                Value = value
+            };
+        }
+
+        public static IEnumerable<PlcWriteReference> FromRoot(string root, params KeyValuePair<string,object>[] variables)
+        {
+            foreach (var variable in variables)
+            {
+                yield return FromAddress($"{root}.{variable.Key}", variable.Value);
+            }
+        }
+
+
+    }
+}
