b2365d714dbc0d49ec26890cf0c79fdc57f01c52
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2019-02-22 17:45:24 +0100

    started adding benchmarks

---
+++ Papper/test/Benchmarks/Benchmarks.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using Papper;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [ClrJob(baseline:true), CoreJob]
+    [RankColumn]
+    public class Benchmarks
+    {
+
+        [Benchmark]
+        public PlcReadReference ParseReadReference() => PlcReadReference.FromAddress($"DB_Safety2.SafeMotion.Header.States.ChecksumInvalid");
+
+        [Benchmark]
+        public PlcWriteReference ParseWriteReference() => PlcWriteReference.FromAddress($"DB_Safety2.SafeMotion.Header.States.ChecksumInvalid", true);
+
+
+    }
+}
---
+++ Papper/test/Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hint:   Disable assembly signing!!");
+            var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
