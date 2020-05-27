3873830260a1d6740f99cf772a162426c2913fee
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2020-05-27 10:45:11 +0200

    added benchmark

---
+++ Papper/test/Benchmarks/ParalleVsSeqBenchmarks.cs
@@ -0,0 +1,105 @@
+﻿using BenchmarkDotNet.Attributes;
+using Benchmarks.Mappings;
+using Papper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [ClrJob(baseline: true), CoreJob]
+    [RankColumn]
+    public class ParalleVsSeqBenchmarks
+    {
+        private List<DataPack> _reads;
+        private List<DataValueTMP> _results;
+
+        [Params(1, 2, 4, 5, 6, 12)]
+        public int NumberOfReds { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _reads = new List<DataPack>();
+            _results = new List<DataValueTMP>();
+            for (int i = 0; i < 30; i++)
+            {
+                _reads.Add(new DataPack
+                {
+
+                });
+                _results.Add(new DataValueTMP());
+            }
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+        }
+
+
+        [Benchmark]
+        public void CreateResultsParallel()
+        {
+
+            _reads.Take(NumberOfReds).AsParallel().Select((item, index) =>
+            {
+                if (index < NumberOfReds)
+                {
+                    var result = _results.ElementAt(index);
+                    item.ApplyData(result.Data);
+                    item.ExecutionResult = result.ReturnCode == ItemResponseRetValue.Success ? ExecutionResult.Ok : ExecutionResult.Error;
+                }
+                else
+                {
+                    item.ExecutionResult = ExecutionResult.Error;
+                }
+                return true;
+            }).ToList();
+
+        }
+
+        [Benchmark]
+        public void CreateResultsSync()
+        {
+
+            _reads.Take(NumberOfReds).Select((item, index) =>
+            {
+                if (index < NumberOfReds)
+                {
+                    var result = _results.ElementAt(index);
+                    item.ApplyData(result.Data);
+                    item.ExecutionResult = result.ReturnCode == ItemResponseRetValue.Success ? ExecutionResult.Ok : ExecutionResult.Error;
+                }
+                else
+                {
+                    item.ExecutionResult = ExecutionResult.Error;
+                }
+                return true;
+            }).ToList();
+
+        }
+
+    }
+
+
+    internal enum ItemResponseRetValue : byte
+    {
+        Reserved = 0,
+        HardwareFault = 1,
+        AccessFault = 3,
+        OutOfRange = 5,
+        NotSupported = 6,
+        SizeMismatch = 7,
+        DataError = 10,
+        Success = 255
+    }
+
+    internal class DataValueTMP
+    {
+        public Memory<byte> Data { get; } = new byte[100];
+        public ItemResponseRetValue ReturnCode { get; } = ItemResponseRetValue.Success;
+    }
+}
--- Papper/test/Benchmarks/Program.cs
+++ Papper/test/Benchmarks/Program.cs
@@ -8,7 +8,8 @@ namespace Benchmarks
         private static void Main(string[] args)
         {
             Console.WriteLine("Hint:   Disable assembly signing!!");
-            var summary = BenchmarkRunner.Run<Benchmarks>();
+            //var summary = BenchmarkRunner.Run<Benchmarks>();
+            var summary = BenchmarkRunner.Run<ParalleVsSeqBenchmarks>();
         }
     }
 }
