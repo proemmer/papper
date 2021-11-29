f16b9300d597010c2039cc19e41c5faa2eb26504
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2021-11-29 16:05:51 +0100

    Added MetaTagAttribute to add additional stuff to some classes

---
+++ Papper/src/Papper/Attributes/MetaTagAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Papper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class MetaTagAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public object Value { get; private set; }
+        public Type ValueType { get; private set; }
+
+        public MetaTagAttribute(string name, object value)
+        {
+            Name = name;
+            Value = value;
+            ValueType = value.GetType();
+        }
+    }
+}
